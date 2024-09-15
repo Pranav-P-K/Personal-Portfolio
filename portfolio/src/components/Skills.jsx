5d5b88ed9b0ca36294a3f8566ee0b52aa705d9f7
Pranav P K <pranavpk1519@gmail.com>
2024-09-15 16:46:36 +0530

    Skills section modified

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -11,34 +11,116 @@ const Skills = () => {
         <button onClick={() => setActive('languages')}>LANGUAGES</button>
         <button onClick={() => setActive('frontend')}>FRONTEND</button>
         <button onClick={() => setActive('backend')}>BACKEND</button>
+        <button onClick={() => setActive('softwares')}>SOFTWARES</button>
       </div>
-        <div className={`skills-content ${active === 'languages' ? 'active' : ''}`}>
+      <div className={`skills-content ${active === 'languages' ? 'active' : ''}`}>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/python--v1.png" alt="python" />
+          <p>Python</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/java-coffee-cup-logo--v1.png" alt="java" />
+          <p>Java</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/c-programming.png" alt="c" />
+          <p>C</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/c-plus-plus-logo.png" alt="c++" />
+          <p>C++</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/dart.png" alt="dart" />
+          <p>Dart</p>
         </div>
-        <div className={`skills-content ${active === 'frontend' ? 'active' : ''}`}>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/500/c-sharp-logo.png" alt="c-sharp" />
+          <p>C#</p>
+        </div>
+      </div>
+      <div className={`skills-content ${active === 'frontend' ? 'active' : ''}`}>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/html-5--v1.png" alt="html5" />
+          <p>HTML5</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/css3.png" alt="css3" />
+          <p>CSS3</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/javascript--v1.png" alt="javascript" />
+          <p>JavaScript</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/typescript.png" alt="typescript" />
+          <p>TypeScript</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/office/500/react.png" alt="react-js"/>
+          <p>ReactJS</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/nextjs.png" alt="next-js"/>
+          <p>NextJS</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/redux.png" alt="redux"/>
+          <p>Redux</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/fluency/500/tailwind_css.png" alt="tailwind-css"/>
+          <p>Tailwind CSS</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/bootstrap--v2.png" alt="bootstrap"/>
+          <p>Bootstrap</p>
+        </div>
+        <div className="skill-item">
           <img src="https://img.icons8.com/color/500/sass.png" alt="SASS"/>
-
+          <p>SASS</p>
         </div>
-        <div className={`skills-content ${active === 'backend' ? 'active' : ''}`}>
+      </div>
+      <div className={`skills-content ${active === 'backend' ? 'active' : ''}`}>
+        <div className="skill-item">
           <img src="https://img.icons8.com/fluency/500/node-js.png" alt="node-js" />
-          <img src="https://img.icons8.com/fluency/500/express-js.png" alt="express-js" />
-          <img src="https://img.icons8.com/color/500/mongodb.png" alt="mongodb" />
-          <img src="https://img.icons8.com/color/500/mysql-logo.png" alt="mysql" />
-          <img src="https://img.icons8.com/color/500/google-firebase-console.png" alt="firebase"/>
+          <p>NodeJS</p>
         </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/fluency/500/express-js.png" alt="express-js" />
+          <p>ExpressJS</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/500/mongodb.png" alt="mongodb" />
+          <p>MongoDB</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/500/mysql-logo.png" alt="mysql" />
+          <p>MySQL</p>
+        </div>
+      </div>
+      <div className={`skills-content ${active === 'softwares' ? 'active' : ''}`}>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/500/visual-studio-code-2019.png" alt="vs-code" />
+          <p>VS Code</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/500/git.png" alt="git" />
+          <p>Git</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/deco/500/unity.png" alt="unity" />
+          <p>Unity</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/fluency/500/jupyter.png" alt="jupyter" />
+          <p>Jupyter</p>
+        </div>
+        <div className="skill-item">
+          <img src="https://img.icons8.com/color/48/flutter.png" alt="flutter" />
+          <p>Flutter</p>
+        </div>
+      </div>
     </section>
   );
 };
